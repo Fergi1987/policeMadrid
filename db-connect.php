5bdcb76fd85b1f5e16a8ffb81327c28b58e68fbf
RaulmartinWAG <raul.martin@wolfavionic.com>
2024-04-23 13:37:51 +0200

    Quitada conexión bbdd

--- db-connect.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$conn = mysqli_connect("127.0.0.1","u665669770_landing","PW5kZ729jLhI4junUu0wKhYJaGSbPH","u665669770_landing");
- 
-if(!$conn){
-	die("Connection error: " . mysqli_connect_error());	
-}
-?>
